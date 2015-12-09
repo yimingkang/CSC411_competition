1b4a72f98f17cef1785a3ad64a248e8e42951181
Ryan Kang <yimingkang@hotmail.com>
2015-12-09 00:44:40 -0500

    Push out changed with normalization

--- data/SVM_ryan.py
+++ data/SVM_ryan.py
@@ -33,26 +33,24 @@ def get_training_data(normalize=True):
     if normalize:
         train_img = preprocessing.scale(train_img * 1.0, axis=1)
 
-    print "IMAGE#100: ", train_img[100]
-    print "IMAGE#-1: ", train_img[-1]
+    show_img(train_img[0])
     return train_img, train['tr_labels']
 
 def train_SVM():
-    train_img, train_labels = get_training_data(normalize=False)
+    train_img, train_labels = get_training_data(normalize=True)
     n_images, _ = train_img.shape
-    experiment = ["linear"]
+    experiment = ["linear", "poly", "sigmoid"]
 
     # iterate through all linear
     if "linear" in experiment:
-        #for c in [0.001, 0.01, 0.1, 0.2, 0.5, 0.8, 0.9, 1, 10, 20, 100]:
-        clf = svm.SVC(kernel='linear', C=1)
-        scores = cross_validation.cross_val_score(clf, train_img, np.reshape(train_labels, (n_images, )) , cv=5)
-        print "linear: ", scores
-        print "linear: ", scores.mean()
+        for c in [1, 10, 100]:
+            clf = svm.SVC(kernel='linear', C=c)
+            scores = cross_validation.cross_val_score(clf, train_img, np.reshape(train_labels, (n_images, )) , cv=5)
+            print "linear mean: ", scores.mean()
 
     # iterate through all poly
     if "poly" in experiment:
-        for deg in xrange(1, 3):
+        for deg in xrange(1, 6):
             clf = svm.SVC(kernel='poly', degree=deg)
             scores = cross_validation.cross_val_score(clf, train_img, np.reshape(train_labels, (n_images, )) , cv=5)
             print "POLY: ", deg,  " MAX: ", scores.max(), " MIN: ", scores.min(), " MEAN: ", scores.mean()
@@ -61,7 +59,7 @@ def train_SVM():
     if "sigmoid" in experiment:
         clf = svm.SVC(kernel='sigmoid')
         scores = cross_validation.cross_val_score(clf, train_img, np.reshape(train_labels, (n_images, )) , cv=5)
-        print scores.mean()
+        print "Sigmoid mean: ", scores.mean()
     return clf
 
 def classify(classifier, samples):
@@ -73,6 +71,9 @@ def classify_pub_test(classifier):
     print test["public_test_images"].shape
     #return classifier.predict(samples)
 
+def show_img(img):
+    plt.imshow(img.reshape(32, 32))
+    plt.show()
 
 def main():
     #preproc()
