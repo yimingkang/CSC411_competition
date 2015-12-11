140a4a50e0dedf415c7e00b1f019ea439fde39f4
Ryan Kang <yimingkang@hotmail.com>
2015-12-11 16:13:48 -0500

    Add aaron's file

---
+++ submit/SVM.py
@@ -0,0 +1,63 @@
+def train_SVM():
+    train = scipy.io.loadmat('./filtered_normalized.mat')
+    train_original = scipy.io.loadmat('labeled_images.mat')
+
+    print "Shape of tr_images is: ", train["tr_images"].shape
+    # (x, y, n_images) = train["tr_images"].shape
+    (n_images, dim) = train["tr_images"].shape
+
+    # train_img = np.reshape(np.swapaxes(train["tr_images"], 0, 2), (n_images, x * y))
+    train_img = train['tr_images']
+    #plt.imshow(np.swapaxes(np.reshape(train_img[0], (y, x)), 0, 1), cmap=pylab.gray())
+    #plt.show()
+
+    clf = svm.SVC(kernel='poly', degree=2)
+    # clf = svm.SVC(kernel='linear', C=10)
+    clf.fit(train_img, np.reshape(train_original["tr_labels"], (n_images, )))
+    # scores = cross_validation.cross_val_score(clf, train_img, np.reshape(train_original["tr_labels"], (n_images, )) , cv=3)
+
+    # print scores
+    # print scores.mean()
+    return clf
+
+def classify(classifier, samples):
+    return classifier.predict(samples)
+
+def classify_pub_test(classifier):
+    pub_test = scipy.io.loadmat('./public_test_normalized.mat')
+    hid_test = scipy.io.loadmat('./hidden_test_images_filtered_normalized.mat')
+    (n_images, dim) = pub_test["public_test_images"].shape
+    test_img = pub_test['public_test_images']
+    pub_res = list(classifier.predict(test_img))
+    (n_images, dim) = hid_test["hidden_img"].shape
+    test_img = hid_test['hidden_img']
+    hid_res = list(classifier.predict(test_img))
+    return pub_res+hid_res
+
+def main():
+    # preproc()
+    print "Training SVM..."
+    classifier = train_SVM()
+    print "Classifing..."
+    cls_res_list = classify_pub_test(classifier)
+    print cls_res_list
+    with open('submit_normalized_poly2.csv', 'w') as f:
+        f.write('Id,Prediction\n')
+        index = 1
+        for pred in cls_res_list:
+            f.write('%d,%d\n'%(index, pred))
+            index += 1
+        while index<=1253:
+            f.write('%d,0\n'%(index))
+            index+=1
+
+if __name__ == '__main__':
+    print "Importing libs..."
+    import scipy.io
+    import pylab
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from sklearn import svm
+    from sklearn import cross_validation
+    print "Done!"
+    main()
